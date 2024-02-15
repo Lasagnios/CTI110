52430d3b7a2603d2805a4d92ded97f5c153bcfad
Lasagnios <160173087+Lasagnios@users.noreply.github.com>
2024-02-15 14:36:33 -0500

    Add files via upload

---
+++ P1LAB1V2_NoriegaEnrique.py
@@ -0,0 +1,26 @@
+### Enrique Noriega
+### 2/15/2024
+### P1LAB1
+### This program will display my name in a welcome message
+##
+##
+###get the first name from the user and assign it to a variable
+##
+##fname = input("Enter your first name: ")
+##print(fname)
+###get the last name from the user and assign it to a variable
+##
+##lname = input("Enter your last name: ")
+##print(lname)
+###print out the names in a welcome message
+##
+##print("Hello,",fname,lname+"! Welcome to CTI-110")
+
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+
+#write a math statement with above numbers
+num3= num1 + num2
+#+here is the concatenation operator
+print(str(num1)+ "+" + str(num2)+ '=' + str(num3))
+
---
+++ P1LAB1_NoriegaEnrique.py
@@ -0,0 +1,17 @@
+# Enrique Noriega
+# 2/15/2024
+# P1LAB1
+# This program will display my name in a welcome message
+
+
+#get the first name from the user and assign it to a variable
+
+fname = input("Enter your first name: ")
+print(fname)
+#get the last name from the user and assign it to a variable
+
+lname = input("Enter your last name: ")
+print(lname)
+#print out the names in a welcome message
+
+print("Hello,",fname,lname+"! Welcome to CTI-110")
