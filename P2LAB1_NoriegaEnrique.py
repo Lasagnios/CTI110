c99a9564c397488515da09aef6e8334f1b112c88
Lasagnios <160173087+Lasagnios@users.noreply.github.com>
2024-02-27 14:09:17 -0500

    Add files via upload

---
+++ P2LAB1_NoriegaEnrique.py
@@ -0,0 +1,19 @@
+#Enrique Noriega
+#2/27/2024
+#P2LAB1
+#A calculator to calculate measurements of circles
+
+import math
+radius= float(input("What is the radius of the circle? "))
+diameter = 2*radius
+print()
+print(f"The diameter of the circle is {diameter:.1f}")
+circumference = 2*3.14*radius
+print()
+print(f"The circumference of the circle is {circumference:.2f}")
+area= 3.14*math.pow(radius,2)
+print()
+print(f"The area of the circle is {area:.3f}")
+
+
+      
